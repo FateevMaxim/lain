e019b1eaee832195ce32b9bb2df8fd906c961c8d
maximfateev <maxim.f@rocketfirm.com>
2024-04-27 13:06:22 +0500

    fix

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -102,7 +102,7 @@
                 </div>
                 <form method="POST" action="{{ route('users-filter') }}" id="userFilterForm">
                     @csrf
-                <div class="grid grid-cols-6">
+                <div class="grid grid-cols-2 md:grid-cols-6">
                         <div class="mr-2">
                             <label for="status" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Выберите статус</label>
                             <select name="status" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
@@ -137,11 +137,11 @@
                             </select>
                         </div>
 
-                    <div style="align-content: end;">
-                        <button type="submit" class="focus:outline-none text-white bg-[#31c48d] hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2">Показать</button>
+                    <div style="align-content: end;" class="mr-2 mt-2 md:mt-0">
+                        <button type="submit" class="w-full focus:outline-none text-white bg-[#31c48d] hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2">Показать</button>
                     </div>
-                        <div style="align-content: end;">
-                            <a href="" id="a"><button type="button" id="excel" class="text-white ml-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Экспорт в Excel</button></a>
+                        <div style="align-content: end;" class="mr-2 mt-2 md:mt-0">
+                            <a href="" id="a"><button type="button" id="excel" class="w-full text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Экспорт в Excel</button></a>
                         </div>
 
                 </div>
