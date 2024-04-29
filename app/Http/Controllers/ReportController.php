a68f97259eaaab75ec60740a541a2d4927b0290b
maximfateev <maxim.f@rocketfirm.com>
2024-04-29 11:50:07 +0500

    report fix

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -10,29 +10,40 @@ use Illuminate\Http\Request;
 class ReportController extends Controller
 {
     public function getTrackReportPage(){
+        $cities = City::query()->select('title')->get();
         $config = Configuration::query()->select('title_text')->first();
         $city = '';
         $date = '';
         $status = '';
-        return view('report', compact('config', 'city', 'date', 'status'));
+        return view('report', compact('cities', 'config', 'city', 'date', 'status'));
     }
 
     public function getTrackReport(Request $request){
 
+        $city = '';
         $date = '';
+        $status = '';
         $query = TrackList::query()
-            ->select('track_code', 'status');
+            ->select('track_code', 'status', 'city');
         if ($request->date != null){
             $query->whereDate('to_client', $request->date);
             $date = $request->date;
         }
-        $query->where('status', 'LIKE', 'Отправлено в Ваш город');
+        if ($request->city != 'Выберите город'){
+            $query->where('city', 'LIKE', $request->city);
+            $city = $request->city;
+        }
+        if ($request->status != 'Выберите статус'){
+            $query->where('status', 'LIKE', $request->status);
+            $status = $request->status;
+        }
+        $cities = City::query()->select('title')->get();
         $tracks = $query->with('user')->get();
         $count = $tracks->count();
         $config = Configuration::query()->select('title_text')->first();
 
 
-        return view('report', compact('tracks',  'config', 'date', 'count'));
+        return view('report', compact('tracks', 'cities', 'config', 'city', 'date', 'status', 'count'));
 
     }
 }
