a72078094fce1130bfe9a9cc2055d198a993d681
maximfateyev <maxim.f@rocketfirm.com>
2024-06-03 11:44:32 +0500

    title fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Nur-777 - доставка товаров из Китая</title>
+        <title>Lain Cargo - доставка товаров из Китая</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
